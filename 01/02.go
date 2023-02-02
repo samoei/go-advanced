c76cd4b7e1fb875f5a07ad1947ec678e731be699
Phil Samoei <phil@samphiltech.com>
2023-02-02 12:07:48 +0100

    removed goland files

--- 01/02.go
+++ 01/02.go
@@ -1,1 +1,26 @@
 package main
+
+import "fmt"
+
+func main() {
+
+	channel1 := make(chan string)
+	channel2 := make(chan string)
+
+	go func() {
+		channel1 <- "Message from channel 1"
+	}()
+
+	go func() {
+		channel2 <- "Message from channel 2"
+	}()
+
+	select {
+	case channel1Message := <-channel1:
+		fmt.Println(channel1Message)
+	case channel2Message := <-channel2:
+		fmt.Println(channel2Message)
+
+	}
+
+}
